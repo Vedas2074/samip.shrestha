eb0af5d1624422f965aa3fd8841da91bb3188c3a
samip shrestha <samipstha114@gmail.com>
2020-12-10 08:43:36 +0545

    employee detail added and design form

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,33 +1,25 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
 using EmployeeManagement.Models;
 
 namespace EmployeeManagement.Controllers{
     public class EmployeeController: Controller{
         public IActionResult Index()
         {   
-            Employee employee1 = new Employee()
-            {
-                Id =1,
-                FirstName = "Rem",
-                LastName ="Hari", 
-                Address = "Ktm",
-                Age = 23,
-                Salary = 1247
-
-            };
-            Employee employee2 = new Employee()
-            {
-                Id = 2,
-                FirstName = "Samip",
-                LastName ="Stha", 
-                Address ="Bkt",
-                Age = 32,
-                Salary = 123
-
-            };
-            List<Employee> employees =new List<Employee>(){employee1,employee2};
+            var employees = Employee.GetEmployee();
             return View(employees);
+        }
+        public ActionResult Detail(int Id)
+        {
+            var employees = Employee.GetEmployee();
+            var employee=employees.FirstOrDefault(x=>x.Id==Id);
+            return View(employee);
+        }
+        public ActionResult Create()
+        {
+            return View();
         }
     }
 }
--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 namespace EmployeeManagement.Models{
 
 public class Employee{
@@ -7,5 +8,31 @@ public class Employee{
     public string Address { get; set;}
     public byte Age { get; set;}
     public float Salary { get; set;}
+     
+     public static List<Employee> GetEmployee()
+     {
+     Employee employee1 = new Employee()
+            {
+                Id =1,
+                FirstName = "Rem",
+                LastName ="Hari", 
+                Address = "Ktm",
+                Age = 23,
+                Salary = 1247
+
+            };
+            Employee employee2 = new Employee()
+            {
+                Id = 2,
+                FirstName = "Samip",
+                LastName ="Stha", 
+                Address ="Bkt",
+                Age = 32,
+                Salary = 123
+
+            };
+            List<Employee> employees =new List<Employee>(){employee1,employee2};
+            return employees;
+     }
 }
 }
