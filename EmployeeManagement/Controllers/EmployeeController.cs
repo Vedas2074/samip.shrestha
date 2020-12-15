9e5ef74fe59c2887791d74d6d6563d9bf63c9b04
samip shrestha <samipstha114@gmail.com>
2020-12-15 15:35:52 +0545

    fetch data from db

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -3,18 +3,23 @@ using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
 using EmployeeManagement.Models;
+using EmployeeManagement.Data;
 
 namespace EmployeeManagement.Controllers{
     public class EmployeeController: Controller{
+        private readonly EMContext db;
+        public EmployeeController(EMContext _db)
+        {
+            db = _db;
+        }
         public IActionResult Index()
         {   
-            var employees = Employee.GetEmployee();
+            var employees = db.Employees.ToList();
             return View(employees);
         }
         public ActionResult Detail(int Id)
         {
-            var employees = Employee.GetEmployee();
-            var employee=employees.FirstOrDefault(x=>x.Id==Id);
+            var employee = db.Employees.Find(Id);
             return View(employee);
         }
         [HttpGet]//default ma hunxa
